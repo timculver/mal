bc35c6a37a62c466567136439c180d804aafbf32
ticu <ticu@ticu-macbookpro.roam.corp.google.com>
2015-03-28 13:04:37 -0400

    Remove moved files

--- cpp/core.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CORE_HPP
-#define CORE_HPP
-
-#include "env.hpp"
-
-Env* core();
-
-#endif
--- cpp/eval.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef EVAL_HPP
-#define EVAL_HPP
-
-#include "types.hpp"
-#include "env.hpp"
-
-MalType* EVAL(MalType* s, Env* env);
-
-#endif
